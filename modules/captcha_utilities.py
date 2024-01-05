4ad66a2142d8b1db1fedf20cd1d77070ea41669c
NyaShinn1204 <team.nyarara@gmail.com>
2024-01-05 12:21:31 +0900

    Test Get_balance For Anticaptcha

--- modules/captcha_utilities.py
+++ modules/captcha_utilities.py
@@ -1,2 +1,17 @@
+import requests
+
 def get_balance(provider, apikey):
-    print(provider, apikey)
+    resp = requests.post(f"https://api.{provider}/getBalance", json={"clientKey": {apikey}})
+    if resp.status_code == 200:
+        balance = resp.json()["balance"]
+        if balance == 0.0:
+            print(f"[+] Working Key: {apikey[:10]}  But Balance 0.0$")
+        else:
+            print(f"[+] Working Key: {apikey[:10]}  Balance: {balance}$")
+        return resp.json()["balance"]
+    elif "ERROR_KEY_DOES_NOT_EXIST" in resp.text:
+        print(f"[-] Invalid Key: {apikey[:10]}")
+        return 0.0
+    else:
+        print(f"[-] Invalid Key Or Exception Error   Key: {apikey[:10]} Status Code: {resp.status_code}")
+        return 0.0
