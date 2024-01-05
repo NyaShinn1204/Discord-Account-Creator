98eb4866ff05c7fe7ba52f304c127c94f8f0ae90
NyaShinn1204 <team.nyarara@gmail.com>
2024-01-05 12:13:27 +0900

    Change Print Summon

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def check_version():
     get_version = requests.get("https://raw.githubusercontent.com/NyaShinn1204/Discord-Account-Creator/dev-v0.0.1/version").text.split("\n")[0]
     now_version = open("version","r").read().splitlines()[0]
     if now_version == get_version:
-      printl("info", "Using Latest Version: "+now_version)
+      printl("info", f"Using Latest Version: {now_version}")
     else:
-      printl("error", "You are using an older version, now version: "+now_version)
-      printl("error", "Latest Version: "+get_version)
+      printl("error", f"You are using an older version, now version: {now_version}")
+      printl("error", f"Latest Version: {get_version}")
   except:
     printl("error", "Failed to Check Version")
     
