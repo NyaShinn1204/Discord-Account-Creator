5b75c722d0037430e77eaab1d3cb79c983ada136
NyaShinn1204 <team.nyarara@gmail.com>
2024-01-06 20:55:21 +0900

    Add Test Captcha Service

--- modules/captcha_utilities.py
+++ modules/captcha_utilities.py
@@ -1,5 +1,7 @@
-import requests
+import requests, httpx, time, yaml
 from modules.console import printl
+
+config = yaml.safe_load((open("config.yml", encoding="utf-8")))
 
 def get_balance(provider, apikey):
     try:
@@ -15,3 +17,39 @@ def get_balance(provider, apikey):
     except:
         printl("error", "Failed to get captcha balance")
         return 0.0
+    
+def solve_captcha(sitekey, siteurl, address, port, username, password):
+    client = httpx.Client(timeout=httpx.Timeout(20.0, read=20.0))
+    startedSolving = time.time()
+    url = "https://api.anti-captcha.com/createTask"
+    data = {
+        "clientKey": config["captcha_solver"]["apikey"],
+        "task":
+        {
+            "type": "HCaptchaTask",
+            "websiteURL": siteurl,
+            "websiteKey": sitekey,
+            "proxyType": "http",
+            "proxyAddress": address,
+            "proxyPort": port,
+            "proxyLogin": username,
+            "proxyPassword": password
+        }
+    }
+    response = client.post(url,json=data)
+    if response.json()['errorId'] == 0:
+        task_id = response.json()['taskId']
+        url = "https://api.anti-captcha.com/getTaskResult"
+        data = {
+            "clientKey": config["captcha_solver"]["apikey"],
+            "taskId": task_id
+        }
+        response = client.post(url,json=data)
+        while response.json()['status'] == 'processing':
+            time.sleep(3)
+            response = client.post(url,json=data)
+        printl("info", f"Solved | {response.json()['solution']['gRecaptchaResponse'][-32:]} In {round(time.time()-startedSolving)}s")
+        return response.json()['solution']['gRecaptchaResponse']
+    else:
+        printl("error", f"[{(response.json()['errorDescription'])}")
+        return False
--- modules/creator.py
+++ modules/creator.py
@@ -1,15 +1,23 @@
-import yaml, random, base64, json, string
+import yaml, random, base64, json, string, requests, time, re
 from modules.utilities import get_session, get_cookies, get_fingerprint, get_buildnum, get_username, get_globalname, get_email, get_password, format_proxy
+from modules.captcha_utilities import solve_captcha
 from modules.console import printl
 from modules.random_agent import random_agent
+from bs4 import BeautifulSoup
 
 def creator(proxie):
     config = yaml.safe_load((open("config.yml", encoding="utf-8")))
     formated_proxy = format_proxy(proxie)
     if formated_proxy[0] == 1:
-        printl("info", f"Running on {formated_proxy[1][:7] + '*' * (len(formated_proxy[1]) - 7)}:{formated_proxy[2]}")
+        proxy_host = formated_proxy[1]
+        proxy_port = formated_proxy[2]
+        printl("info", f"Running on {proxy_host[:7] + '*' * (len(proxy_host) - 7)}:{proxy_port}")
     if formated_proxy[0] == 2:
-        printl("info", f"Running Proxie of {formated_proxy[3][:7] + '*' * (len(formated_proxy[3]) - 7)}:{formated_proxy[4]} | {formated_proxy[1][:3] + '*' * (len(formated_proxy[1]) - 3)}:{formated_proxy[2][:3] + '*' * (len(formated_proxy[2]) - 3)}")
+        proxy_username = formated_proxy[1]
+        proxy_password = formated_proxy[2]
+        proxy_host = formated_proxy[3]
+        proxy_port = formated_proxy[4]
+        printl("info", f"Running Proxie of {proxy_host[:7] + '*' * (len(proxy_host) - 7)}:{proxy_port} | {proxy_username[:3] + '*' * (len(proxy_username) - 3)}:{proxy_password[:3] + '*' * (len(proxy_password) - 3)}")
     session = get_session()
     cookies = get_cookies(session)
     fingerprint = get_fingerprint(session)
@@ -29,7 +37,7 @@ def creator(proxie):
     else:
         email = f"{''.join(random.choice(string.ascii_letters + string.digits) for i in range(random.randint(15)))}@gmail.com" 
     password = get_password()
-        
+
     # Headers
     ##ここから
     agent_string = random_agent()
@@ -96,3 +104,80 @@ def creator(proxie):
         "username": username,
     }
     ##ここまで
+    
+    response = session.post('https://discord.com/api/v9/auth/register', headers=headers, proxy=f"http://{proxie}", json=payload, cookies=cookies)
+    print(response.status_code, response.text)
+    if response.status_code == 201:
+        token = response.json()['token']
+        printl("info", f"Success Create Account {email}:{password}:{token}")
+    elif response.status_code == 400:
+        printl("info", "Captcha Solving...")
+        captcha_sitekey = response.json()["captcha_sitekey"]
+        captcha_result = solve_captcha(captcha_sitekey, "https://discord.com/register", proxy_host, proxy_port, proxy_username, proxy_password)
+        if captcha_result:
+            headers['X-Captcha-Key'] = captcha_result
+            response = session.post('https://discord.com/api/v9/auth/register', headers=headers, proxy=f"http://{proxie}", json=payload, cookies=cookies)
+            print(response.status_code, response.text)
+            if response.status_code == 200 or response.status_code == 201:
+                token = response.json()['token']
+                printl("info", f"Success Create Account {email}:{password}:{token}")
+                headers['Authorization'] = token
+                headers.pop('X-Captcha-Key')
+            else:
+                return
+        else:
+            return
+        
+        
+    if config["email_verify"]["enable"] == True:
+        poipoi_session = requests.session()
+        poipoi_session.cookies.set('cookie_csrf_token', config["email_verify"]["m.kuku.lu_token"])
+        poipoi_session.cookies.set('cookie_sessionhash', config["email_verify"]["m.kuku.lu_sessionhash"])
+        while True:
+            response = poipoi_session.get(f'https://m.kuku.lu/recv._ajax.php?&q={email} メールアドレスを確認してください&csrf_token_check={config["email_verify"]["m.kuku.lu_token"]}')
+            soup = BeautifulSoup(response.text, 'html.parser')
+            if soup.find('span', attrs={'class':'view_listcnt'}).contents[0] == '1':
+                break
+            time.sleep(2)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        mail_element = soup.find('div', attrs={'class':'main-content'}).find('div', attrs={'style':'z-index:99;'})
+        script_element = mail_element.parent.find_all('script')[2]
+        parsed_javascript = re.findall(r'\'.*\'', script_element.string)
+        num = parsed_javascript[1].split(',')[0].replace('\'', '')
+        key = parsed_javascript[1].split(',')[1].replace('\'', '').replace(' ', '')
+
+        response = poipoi_session.post('https://m.kuku.lu/smphone.app.recv.view.php', data={'num':num, 'key':key})
+        soup = BeautifulSoup(response.text, 'html.parser')
+        verify_redirect_url = soup.find('a', text='\n            Verify Email\n          ').attrs['href']
+        response = requests.get(verify_redirect_url, headers=headers, proxies=f"http://{proxie}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_element = soup.find('script')
+        verify_url = script_element.contents[0].replace('\n', '').replace('\t', '').replace('setTimeout(function(){location.href = "', '').replace('";}, 1);', '')
+        response = requests.get(verify_url, headers=headers, proxies=f"http://{proxie}")
+        verify_token = response.request.url.replace('https://discord.com/verify#token=', '')
+        request_data = {"token": verify_token}
+        response = requests.post('https://discord.com/api/v9/auth/verify', headers=headers, proxies=f"http://{proxie}", json=request_data)
+        print(response.status_code, response.text)
+        if response.status_code == 200:
+            token = response.json()['token']
+            print(f'[Email Verified] {email}:{password}:{token}')
+            headers['Authorization'] = token
+        elif response.status_code == 400:
+            if 'captcha_sitekey' in response.json().keys():
+                captcha_sitekey = response.json()['captcha_sitekey']
+                captcha_result = solve_captcha(captcha_sitekey, "https://discord.com/verify", proxy_host, proxy_port, proxy_username, proxy_password)
+                if captcha_result:
+                    headers['X-Captcha-Key'] = captcha_result
+                    response = requests.post('https://discord.com/api/v9/auth/verify', headers=headers, json=request_data, proxies=f"http://{proxie}")
+                    print(response.status_code, response.text)
+                    if response.status_code == 200 or response.status_code == 201:
+                        print(f'[Email Verified] {email}:{password}:{token}')
+                        token = response.json()['token']
+                        headers['Authorization'] = token
+                        headers.pop('X-Captcha-Key')
+                    else:
+                        return
+                else:
+                    return
+            else:
+                return
